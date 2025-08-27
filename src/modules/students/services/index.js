5a740f6ec6bb5cb000557cc01b1709cd85e1daf7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-08-27 12:42:20 +0500

    studentlarni service orqlai export qilib yuborish

---
+++ src/modules/students/services/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  add: require("./_add"),
+  list: require("./_list"),
+  remove: require("./_remove"),
+  edit: require("./_edit"),
+  login: require("./_login"),
+  profile: require("./_profile"),
+  editSelf: require("./_editSelf"),
+  getStudentStats: require("./_get-student-stats"),
+};
