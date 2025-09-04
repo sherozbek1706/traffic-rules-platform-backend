152411faf0f88960125b64c0c0202d486d6f41b5
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-09-04 17:59:20 +0500

    The main index.js file was written, which was given a server run and placed inside additional functions. The knexfile, which is the query language, was developed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const express=require('express');
-const app=express();
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+const express = require("express");
+const cors = require("cors");
+
+const config = require("./shared/config/index");
+
+const app = express();
+const PORT = config.port || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const router = require("./router");
+// app.use("/api/v1", requestLimiter, router);
+app.use("/api/v1", router);
+
+// Error handling
+app.use(require("./shared/errors/handle"));
+
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portida ishga tushdi 🎉`);
 });
