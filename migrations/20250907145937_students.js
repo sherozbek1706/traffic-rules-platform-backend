04b9a73bf23484b34cb28b93ab2a98ec381ddfe6
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2025-09-18 12:01:28 +0500

    Migration has been completed for the students.

---
+++ migrations/20250907145937_students.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("students", (table) => {
+    table.increments("id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("phone_number").notNullable();
+    table.string("username", 64).notNullable().unique();
+    table
+      .integer("group_id")
+      .unsigned()
+      .references("id")
+      .inTable("groups")
+      .onDelete("RESTRICT")
+      .index();
+    table.string("password").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("students");
+};
